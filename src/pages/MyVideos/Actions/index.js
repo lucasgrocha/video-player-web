d622724942cccde3abae3d07c79323a3d4f7df35
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-20 14:45:13 -0300

    add useNavigate to show video button

--- src/pages/MyVideos/Actions/index.js
+++ src/pages/MyVideos/Actions/index.js
@@ -5,14 +5,20 @@ import {
   AiFillEdit as EditIcon,
   AiFillEye as ShowIcon,
 } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 import './styles.css';
 
 function Actions(props) {
+  const navigate = useNavigate();
+
   return (
     <div className="actions-wrapper">
       <div className="action-button">
-        <ShowIcon id="show-icon" />
+        <ShowIcon
+          id="show-icon"
+          onClick={() => navigate(`/videos/${props.id}`)}
+        />
       </div>
 
       <div className="action-button">
