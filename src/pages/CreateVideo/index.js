1a7dc40da414da762d3d716d962f2c1c5041b7fc
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-19 21:06:47 -0300

    add form to createVideo page

--- src/pages/CreateVideo/index.js
+++ src/pages/CreateVideo/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
 
+import { Form, Button } from 'react-bootstrap';
+
 import './styles.css';
 
 function CreateVideo() {
+  function handleCreateVideoSubmit(evt) {
+    evt.preventDefault();
+  }
+
   return (
     <div className="container">
       <div id="create-form-wrapper">
-        <form>
-          <input type="text" />
-        </form>
+        <Form onSubmit={handleCreateVideoSubmit}>
+          <Form.Group>
+            <Form.Label htmlFor="name">Video name</Form.Label>
+            <Form.Control
+              name="name"
+              type="text"
+              placeholder="Enter the video name"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label htmlFor="description">Video Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="3"
+              placeholder="Write the video description"
+              required
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.File
+              accept="video/mp4,video/x-m4v,video/*"
+              label="Select the video file"
+              required
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" style={{ width: '100%' }}>
+            Upload!
+          </Button>
+        </Form>
       </div>
     </div>
   );
