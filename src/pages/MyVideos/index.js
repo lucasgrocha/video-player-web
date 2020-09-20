a14a1971b310435817f9b0b5f9dcce82152faaba
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-09-20 14:38:13 -0300

    display video table if there's data

--- src/pages/MyVideos/index.js
+++ src/pages/MyVideos/index.js
@@ -6,7 +6,7 @@ import Actions from './Actions';
 import './styles.css';
 
 function MyVideos() {
-  const [videos, setVideos] = useState();
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     videosService.myVideos().then((res) => {
@@ -30,37 +30,41 @@ function MyVideos() {
     });
   }
 
+  const table = (
+    <Table striped bordered hover responsive>
+      <thead className="thead-dark">
+        <tr>
+          <th>Video</th>
+          <th>Name</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {videos?.map((video) => (
+          <tr key={video.id}>
+            <td className="align-middle">
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <img
+                  src={`http://192.168.15.11:3000/${video.thumbnail_url}`}
+                  alt={`${video.name}`}
+                  style={{ maxHeight: '10rem' }}
+                />
+              </div>
+            </td>
+            <td className="align-middle">{video.name}</td>
+            <td className="align-middle">
+              <Actions id={video.id} deleteClicked={handleDeleteVideo} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+
   return (
     <div id="my-videos-wrapper">
       <div className="container">
-        <Table striped bordered hover responsive>
-          <thead className="thead-dark">
-            <tr>
-              <th>Video</th>
-              <th>Name</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {videos?.map((video) => (
-              <tr key={video.id}>
-                <td className="align-middle">
-                  <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img
-                      src={`http://192.168.15.11:3000/${video.thumbnail_url}`}
-                      alt={`${video.name}`}
-                      style={{ maxHeight: '10rem' }}
-                    />
-                  </div>
-                </td>
-                <td className="align-middle">{video.name}</td>
-                <td className="align-middle">
-                  <Actions id={video.id} deleteClicked={handleDeleteVideo} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        {videos.length > 0 ? table : <h1>You didn't upload a video yet</h1>}
       </div>
     </div>
   );
